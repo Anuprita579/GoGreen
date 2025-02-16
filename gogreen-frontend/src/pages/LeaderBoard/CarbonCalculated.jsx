ea51dada3a2f7bf5fb8a8ebf89179b8cd7af264a
Anuprita <anupritamhapankar22@gmail.com>
2025-02-16 21:30:39 +0530

    leaderboard

--- gogreen-frontend/src/pages/LeaderBoard/CarbonCalculated.jsx
+++ gogreen-frontend/src/pages/LeaderBoard/CarbonCalculated.jsx
@@ -1,87 +1,119 @@
-import React, { useEffect, useState } from 'react'
-import styles from "./styles.module.scss"
-import Rank from './Rank';
-
+import React, { useEffect, useState } from "react";
+import styles from "./styles.module.scss";
+import Rank from "./Rank";
 
 const CarbonCalculated = () => {
-    const username = sessionStorage.getItem('name');
-    // const discount = sessionStorage.getItem('discount');
-    
-    const[discount,setDiscount]=useState(null);
-    const[reload,setReload]=useState(null);
+  const username = sessionStorage.getItem("name");
+  // const discount = sessionStorage.getItem('discount');
 
-    function calculateCarbonEmissions(distance, mode, innerOption) {
-        const emissionFactors = {
-            dieselcar: 0.171, 
-            petrolcar:0.170,
-            evcar: 0.047,
-            walking: 0,
-            cycling: 0,
-            cngauto: 0.061,
-            petrolauto: 0.061,
-            bike: 0.114,
-            train: 0.035,
-            normalbus: 0.097,
-            evbus: 0.068,
-        };
+  const [discount, setDiscount] = useState(null);
+  const [reload, setReload] = useState(null);
+
+  function calculateCarbonEmissions(distance, mode, innerOption) {
+    const emissionFactors = {
+      dieselcar: 0.171,
+      petrolcar: 0.17,
+      evcar: 0.047,
+      walking: 0,
+      cycling: 0,
+      cngauto: 0.061,
+      petrolauto: 0.061,
+      bike: 0.114,
+      train: 0.035,
+      normalbus: 0.097,
+      evbus: 0.068,
+    };
     // const factorKey = Object.keys(emissionFactors).find(key => key.toLowerCase() === mode.replace(/\s+/g, '').toLowerCase());
     // const factor = emissionFactors[factorKey] || 0;
     //     return distance * factor;
-    const factorKey = Object.keys(emissionFactors).find(key => key.toLowerCase().replace(/\s+/g, '') === mode?.toLowerCase());
-  if (factorKey) {
-    const factor = emissionFactors[factorKey];
-    return distance * factor;
-  }
-  const innerOptionKey = Object.keys(emissionFactors).find(key => key.toLowerCase().replace(/\s+/g, '') === innerOption?.toLowerCase().replace(/\s+/g, ''));
-  if (innerOptionKey) {
-    const innerFactor = emissionFactors[innerOptionKey];
-    return distance * innerFactor;
-  }
-  return 0;
+    const factorKey = Object.keys(emissionFactors).find(
+      (key) => key.toLowerCase().replace(/\s+/g, "") === mode?.toLowerCase()
+    );
+    if (factorKey) {
+      const factor = emissionFactors[factorKey];
+      return distance * factor;
     }
-    
-    const distributedModes = JSON.parse(sessionStorage.getItem('SelectedTransportModes') || '{}');
-    let totalCarbonEmissions = 0;
+    const innerOptionKey = Object.keys(emissionFactors).find(
+      (key) =>
+        key.toLowerCase().replace(/\s+/g, "") ===
+        innerOption?.toLowerCase().replace(/\s+/g, "")
+    );
+    if (innerOptionKey) {
+      const innerFactor = emissionFactors[innerOptionKey];
+      return distance * innerFactor;
+    }
+    return 0;
+  }
 
-    console.log({reloadRender:reload})
+  const distributedModes = JSON.parse(
+    sessionStorage.getItem("SelectedTransportModes") || "{}"
+  );
+  let totalCarbonEmissions = 0;
 
+  console.log({ reloadRender: reload });
 
-    useEffect(()=>{
-      const temp=sessionStorage.getItem('discount');
-      console.log({sessionStorage,temp})
-      setDiscount(temp)
-    },[reload])
+  useEffect(() => {
+    const temp = sessionStorage.getItem("discount");
+    console.log({ sessionStorage, temp });
+    setDiscount(temp);
+  }, [reload]);
 
-      
   return (
     <div className={styles.carbonCalculatedContainer}>
-    <div className={styles.carbonCalculatedCard}>
-        <p className={styles.caption}>{username} your Carbon Emission is </p>
-        {Object.entries(distributedModes).map(([modeId, { distance, title, innerOption }]) => {
-                const emissions = calculateCarbonEmissions(distance, title, innerOption); // Use title for mode
+      <div className={styles.carbonCalulatedBox}>
+        <img
+          src="https://img.freepik.com/free-vector/achievement-concept-illustration_114360-5577.jpg?ga=GA1.1.424564767.1738696865&semt=ais_hybrid"
+          className={styles.leaderBoardImage}
+        />
+        <div className={styles.carbonCalculatedCard}>
+          <p className={styles.caption}>{username} your Carbon Emission is </p>
+          {Object.entries(distributedModes).map(
+            ([modeId, { distance, title, innerOption }]) => {
+              const emissions = calculateCarbonEmissions(
+                distance,
+                title,
+                innerOption
+              ); // Use title for mode
 
-                totalCarbonEmissions += emissions;
+              totalCarbonEmissions += emissions;
 
-                return (
-                    <div key={modeId} className={styles.carbonItem}>
-                        <p className={styles.modeName}>{title}</p> 
-                        <p><span className={styles.subtitle}>Distance:</span> {distance} <span className={styles.units}>km</span></p> 
-                        <p><span className={styles.subtitle}>Carbon Emissions:</span> {emissions.toFixed(2)} <span className={styles.units}>kg CO2e</span></p>
-                    </div>
-                );
-            })}
+              return (
+                <div key={modeId} className={styles.carbonItem}>
+                  <p className={styles.modeName}>{title}</p>
+                  <p>
+                    <span className={styles.subtitle}>Distance:</span>{" "}
+                    {distance} <span className={styles.units}>km</span>
+                  </p>
+                  <p>
+                    <span className={styles.subtitle}>Carbon Emissions:</span>{" "}
+                    {emissions.toFixed(2)}{" "}
+                    <span className={styles.units}>kg CO2e</span>
+                  </p>
+                </div>
+              );
+            }
+          )}
 
-            <div className={styles.totalCarbon}>
-                <p className={styles.totalCarbonTitle}>Total Carbon Emissions</p>
-                <p className={styles.totalCarbonContent}>{totalCarbonEmissions.toFixed(2)} <span> kg CO2e</span></p>
-            </div>  
+          <div className={styles.totalCarbon}>
+            <p className={styles.totalCarbonTitle}>Total Carbon Emissions</p>
+            <p className={styles.totalCarbonContent}>
+              {totalCarbonEmissions.toFixed(2)} <span> kg CO2e</span>
+            </p>
+          </div>
 
-            <p className={styles.discountEligible}>Congratulations {username} you're eligible for <span className={styles.discountNumber}>{(discount * 100).toFixed(2)}%</span> discount </p>
+          <p className={styles.discountEligible}>
+            Congratulations {username} you're eligible for{" "}
+            <span className={styles.discountNumber}>
+              {(discount * 100).toFixed(2)}%
+            </span>{" "}
+            discount{" "}
+          </p>
+        </div>
+      </div>
+
+      <Rank totalCarbonEmissions={totalCarbonEmissions} setReload={setReload} />
     </div>
+  );
+};
 
-        <Rank totalCarbonEmissions={totalCarbonEmissions} setReload={setReload}/>
-    </div>
-  )
-}
-
-export default CarbonCalculated
+export default CarbonCalculated;
--- gogreen-frontend/src/pages/LeaderBoard/Rank.jsx
+++ gogreen-frontend/src/pages/LeaderBoard/Rank.jsx
@@ -1,115 +1,130 @@
-import React, { useEffect, useState } from 'react'
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import styles from "./styles.module.scss"
+import React, { useEffect, useState } from "react";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import styles from "./styles.module.scss";
 
-const Rank = ({ totalCarbonEmissions,setReload }) => {
+const Rank = ({ totalCarbonEmissions, setReload }) => {
+  const [leaderboardEntries, setLeaderboardEntries] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [discount, setDiscount] = useState(0);
+  const username = sessionStorage.getItem("name");
+  const distance = sessionStorage.getItem("distanceData");
+  const scalingFactor = 0.5;
 
-    const [leaderboardEntries, setLeaderboardEntries] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [discount, setDiscount] = useState(0);
-    const username = sessionStorage.getItem('name');
-    const distance = sessionStorage.getItem('distanceData');
-    const scalingFactor = 0.5;
+  useEffect(() => {
+    handleSubmit();
+    fetchLeaderboardData();
+  }, []);
 
-    useEffect(() => {
-        handleSubmit();
-        fetchLeaderboardData();
-    }, [])
+  const handleSubmit = async () => {
+    const username = sessionStorage.getItem("name");
+    const carbonFootprint = totalCarbonEmissions.toFixed(2);
 
-    const handleSubmit = async () => {
-        const username = sessionStorage.getItem('name');
-        const carbonFootprint = totalCarbonEmissions.toFixed(2);
-
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/leaderboard/submitScore`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, carbonFootprint }),
-            });
-            const data = await response.json();
-            console.log(data.message);
-
-        } catch (error) {
-            console.error('Error submitting score:', error);
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_BASE_URL}/api/leaderboard/submitScore`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, carbonFootprint }),
         }
-    };
-
-    const calculateDiscount = (carbonFootprint) => {
-        if (carbonFootprint > 0) {
-            const discountPercentage = (distance / carbonFootprint) * scalingFactor;
-            return discountPercentage;
-        }
-        return 0;
-    };
-    console.log("Discount : ", discount);
-    // sessionStorage.setItem("discount", discount);
-
-    useEffect(()=>{
-        if(discount){
-            sessionStorage.setItem("discount", discount);
-            setReload(new Date());
-        }
-    },[discount])
-
-    const fetchLeaderboardData = async () => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/leaderboard/getScore`);
-            const data = await response.json();
-            console.log('Leaderbord get data :', data);
-            const sortedEntries = [...data].sort((a, b) => a.carbonFootprint - b.carbonFootprint);
-            const rankedEntries = sortedEntries.reduce((acc, entry, index) => {
-                acc.push({ ...entry, rank: index + 1 });
-                return acc;
-            }, []);
-
-            const userEntry = data.find(entry => entry.username === username);
-            if (userEntry) {
-                const calculatedDiscount = calculateDiscount(userEntry.carbonFootprint);
-                setDiscount(calculatedDiscount / 100);
-            }
-
-            setLeaderboardEntries(rankedEntries);
-            setIsLoading(false);
-        } catch (error) {
-            console.error('Error fetching leaderboard data:', error);
-            setIsLoading(false);
-        }
-    };
-
-    if (isLoading) {
-        return <div>Loading leaderboard...</div>;
+      );
+      const data = await response.json();
+      console.log(data.message);
+    } catch (error) {
+      console.error("Error submitting score:", error);
     }
+  };
 
-    return (
-        <div className={styles.leaderboardBox}>
-            <h3 className={styles.leaderboardTitle}>Leaderboard</h3>
-            <div className={styles.leaderboardContainer}>
-                <div
-                    className={styles.leaderboardCardHeading}
-                >
-                    <span className={styles.rankUsername}>RANK</span>
-                    <span className={styles.rankUsername}>USER ID</span>
-                    <span className={styles.rankUsername}> USERNAME</span>
-                    <p className={styles.carbonFootprintText}>CO<sub>2</sub> EMITTED</p>
-                </div>
-                {leaderboardEntries.map((entry) => (
-                    <div
-                        key={entry.rank}
-                        className={styles.leaderboardCard}
-                        style={{ backgroundColor: entry.username.toLowerCase() === sessionStorage.getItem('name').toLowerCase() ? '#F1A203' : '#065C30' }}
-                    >
-                        <span className={styles.rankRounded}>{entry.rank}</span>
-                        <span className={styles.rankUsername}>{entry._id}</span>
-                        <span className={styles.rankUsername}> {entry.username}</span>
-                        <p className={styles.carbonFootprintText}><span className={styles.carbonFootprintValue}>{entry.carbonFootprint.toFixed(2)}</span> kg CO2e</p>
-                    </div>
-                ))}
-            </div>
+  const calculateDiscount = (carbonFootprint) => {
+    if (carbonFootprint > 0) {
+      const discountPercentage = (distance / carbonFootprint) * scalingFactor;
+      return discountPercentage;
+    }
+    return 0;
+  };
+  console.log("Discount : ", discount);
+  // sessionStorage.setItem("discount", discount);
 
+  useEffect(() => {
+    if (discount) {
+      sessionStorage.setItem("discount", discount);
+      setReload(new Date());
+    }
+  }, [discount]);
+
+  const fetchLeaderboardData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_BASE_URL}/api/leaderboard/getScore`
+      );
+      const data = await response.json();
+      console.log("Leaderbord get data :", data);
+      const sortedEntries = [...data].sort(
+        (a, b) => a.carbonFootprint - b.carbonFootprint
+      );
+      const rankedEntries = sortedEntries.reduce((acc, entry, index) => {
+        acc.push({ ...entry, rank: index + 1 });
+        return acc;
+      }, []);
+
+      const userEntry = data.find((entry) => entry.username === username);
+      if (userEntry) {
+        const calculatedDiscount = calculateDiscount(userEntry.carbonFootprint);
+        setDiscount(calculatedDiscount / 100);
+      }
+
+      setLeaderboardEntries(rankedEntries);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching leaderboard data:", error);
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return <div>Loading leaderboard...</div>;
+  }
+
+  return (
+    <div className={styles.leaderboardBox}>
+      <h3 className={styles.leaderboardTitle}>Leaderboard</h3>
+      <div className={styles.leaderboardContainer}>
+        <div className={styles.leaderboardCardHeading}>
+          <span className={styles.rankUsername}>RANK</span>
+          <span className={styles.rankUsername}>USER ID</span>
+          <span className={styles.rankUsername}> USERNAME</span>
+          <p className={styles.carbonFootprintText}>
+            CO<sub>2</sub> EMITTED
+          </p>
         </div>
-    )
-}
+        {leaderboardEntries.map((entry) => (
+          <div
+            key={entry.rank}
+            className={styles.leaderboardCard}
+            style={{
+              backgroundColor:
+                entry.username.toLowerCase() ===
+                sessionStorage.getItem("name").toLowerCase()
+                  ? "#FFD580"
+                  : "#F2F2F2",
+            }}
+          >
+            <span className={styles.rankRounded}>{entry.rank}</span>
+            <span className={styles.rankUsername}>{entry._id}</span>
+            <span className={styles.rankUsername}> {entry.username}</span>
+            <p className={styles.carbonFootprintText}>
+              <span className={styles.carbonFootprintValue}>
+                {entry.carbonFootprint.toFixed(2)}
+              </span>{" "}
+              kg CO2e
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Rank
+export default Rank;
